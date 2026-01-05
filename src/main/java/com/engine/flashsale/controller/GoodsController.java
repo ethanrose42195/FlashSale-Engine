81c0984cfd69823e00c8e1d6ea49e0655f96f1b8
李达 <richard_daves@163.com>
2026-01-05 11:48:49 +0800

    新增基础 Controller

---
+++ src/main/java/com/engine/flashsale/controller/GoodsController.java
@@ -0,0 +1,45 @@
+package com.engine.flashsale.controller;
+
+import com.engine.flashsale.service.GoodsService;
+import com.engine.flashsale.vo.GoodsVo;
+import com.engine.flashsale.vo.RespBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 商品控制器
+ * * @author ethan
+ */
+@RestController
+@RequestMapping("/goods")
+public class GoodsController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    /**
+     * 获取商品列表
+     * @return 标准JSON结果
+     */
+    @GetMapping("/list")
+    public RespBean list() {
+        List<GoodsVo> goodsVoList = goodsService.findGoodsVo();
+        return RespBean.success(goodsVoList);
+    }
+
+    /**
+     * 获取商品详情
+     * @param goodsId 商品ID
+     * @return 标准JSON结果
+     */
+    @GetMapping("/detail/{goodsId}")
+    public RespBean toDetail(@PathVariable Long goodsId) {
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
+        return RespBean.success(goodsVo);
+    }
+}
---
+++ src/main/java/com/engine/flashsale/enums/RespBeanEnum.java
@@ -0,0 +1,30 @@
+package com.engine.flashsale.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @author ethan
+ */
+
+@Getter
+@ToString
+@AllArgsConstructor
+public enum RespBeanEnum {
+
+    // 通用状态
+    SUCCESS(200, "SUCCESS"),
+    ERROR(500, "服务端异常"),
+
+    // 登录模块 5002xx
+    LOGIN_ERROR(500210, "用户名或密码不正确"),
+    MOBILE_ERROR(500211, "手机号码格式不正确"),
+
+    // 秒杀模块 5005xx
+    EMPTY_STOCK(500500, "库存不足"),
+    REPEATE_ERROR(500501, "该商品每人限购一件");
+
+    private final Integer code;
+    private final String message;
+}
